fbd16fc70ca60edf44cd81a282c88a6ff2ee763c
John Pinkerton <jpinkerton88@gmail.com>
2016-06-27 15:53:08 -0400

    add mobile config

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+App.info({
+  id: 'cc.newspring.LaunchTodosExample',
+  name: 'LaunchTodosExample',
+  description: 'Example of using launch',
+  author: 'NewSpring Church',
+  email: 'web@newspring.cc',
+  website: 'https://newspring.cc',
+  version: '0.0.1',
+  buildNumber: '1'
+});
